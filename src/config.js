35715c92e0f07a6e73e62a8e28185adcfcbbc109
Shaun Saker <shaun@aux.co.za>
2018-02-14 06:13:36 +0200

    Update build number

--- src/config.js
+++ src/config.js
@@ -6,10 +6,8 @@ config.version = {
         minor: 0,
         patch: 0,
     },
-    build: 3,
+    build: 4,
 };
-
-config.latencyTimeout = 5;
 
 config.googlePlacesAPIKey = "AIzaSyAbqz9XQOVIFRtl6X1sovgna3SHDJHtKM0";
 
