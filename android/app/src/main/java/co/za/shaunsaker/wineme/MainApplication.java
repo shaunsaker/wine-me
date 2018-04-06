874717432a29e25062e78d6ef1d4ea26fb397185
Shaun Saker <shaun@aux.co.za>
2018-04-06 16:21:29 +0200

    Remove react-native-linear-gradient as dependency

--- android/app/src/main/java/co/za/shaunsaker/wineme/MainApplication.java
+++ android/app/src/main/java/co/za/shaunsaker/wineme/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
