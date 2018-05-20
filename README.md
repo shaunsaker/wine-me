e7394792b52f77347ce2b9d3eb46abc4b19044df
Shaun Saker <shaun@aux.co.za>
2018-05-20 08:47:49 +0200

    Add README and fix bug in NetworkHandler

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+## Notes:
+
+To get the build working, I had to remove the third-party folder from node_modules/react-native/. See this (issue)[https://github.com/facebook/react-native/issues/14382].
--- src/wrappers/NetworkHandler.js
+++ src/wrappers/NetworkHandler.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NetInfo } from "react-native";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import firebase from 'react-native-firebase'
 
 import Analytics from "../analytics";
 
