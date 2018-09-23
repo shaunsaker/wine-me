78a5590c513e01b7e549acd6f185530ad58c4e4f
shaunsaker <shaun@aux.co.za>
2018-09-23 11:20:53 +0200

    Added Label component

---
+++ src/components/Label/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+import styleConstants from '../../styleConstants';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: styleConstants.colors.dividerColor,
+    paddingVertical: styleConstants.dimensions.padding.small / 2,
+    paddingHorizontal: styleConstants.dimensions.padding.small,
+  },
+  icon: {
+    fontSize: styleConstants.fonts.sizes.icon,
+    color: styleConstants.colors.primaryText,
+    marginRight: styleConstants.dimensions.margin.small,
+  },
+  text: {
+    fontSize: styleConstants.fonts.types.small,
+    color: styleConstants.colors.primaryText,
+  },
+});
+
+export default styles;
