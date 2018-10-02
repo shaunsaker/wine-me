ee7f6f194d3186f9e2f1d651a952ee3e10ca5f13
shaunsaker <shaun@aux.co.za>
2018-10-02 14:01:03 +0200

    Fix android photos borderRadius

--- src/components/PhotoList/styles.js
+++ src/components/PhotoList/styles.js
@@ -20,6 +20,8 @@ const styles = StyleSheet.create({
   image: {
     width: IMAGE_SIZE,
     height: IMAGE_SIZE,
+    borderRadius: styleConstants.dimensions.borderRadius,
+    overflow: 'hidden',
   },
 });
 
