26ac7937fa4ebcfa8b5a3018344d5682bdf8e7ff
shaunsaker <shaun@aux.co.za>
2018-10-02 15:50:41 +0200

    Fix SearchAreaLabelList tests

--- src/components/SearchAreaLabelList/__tests__/SearchAreaLabelList.test.js
+++ src/components/SearchAreaLabelList/__tests__/SearchAreaLabelList.test.js
@@ -3,11 +3,14 @@ import renderer from 'react-test-renderer';
 
 import SearchAreaLabelList from '..';
 import SEARCH_AREAS from '../../../mockData/SEARCH_AREAS';
+import utils from '../../../utils';
 
 describe('SearchAreaLabelList', () => {
+  const searchAreasArray = utils.objects.convertObjectToArray(SEARCH_AREAS);
+
   describe('renders', () => {
     it('renders with minimum required props', () => {
-      const component = renderer.create(<SearchAreaLabelList data={SEARCH_AREAS} />);
+      const component = renderer.create(<SearchAreaLabelList data={searchAreasArray} />);
 
       expect(component).toMatchSnapshot();
     });
